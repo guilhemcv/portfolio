966ea5298a4f4d76d8425d1d6d38c4d020a0a810
Guilhem SEYVET <guilhemcvdev@outlook.com>
2021-11-28 00:11:49 +0100

    ajout composant about

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./About.css";
+import idea from "../../assets/images/idea.png";
 
 export function About() {
   return (
     <div className="about">
-      <h1>coucou</h1>
+      <h1 className="about-titre">A Propos</h1>
+      <div className="cercle"><img className="logo-cercle" src={idea} alt={idea} /></div>
     </div>
   )
 }
