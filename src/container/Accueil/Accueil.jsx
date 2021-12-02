fbfc139e238df866eebd344069d90561b2530b5d
Guilhem SEYVET <guilhemcvdev@outlook.com>
2021-12-02 14:12:40 +0100

    ajout cookies

--- src/container/Accueil/Accueil.jsx
+++ src/container/Accueil/Accueil.jsx
@@ -1,43 +1,50 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
-import CookieConsent from "react-cookie-consent";
+import { useNavigate } from 'react-router-dom';
+import CookieConsent from 'react-cookie-consent';
 import './Accueil.css';
 
 export function Accueil() {
   const cookieStyle = {
-    width: "400px",
-    display: "flex",
-    justifyContent: "center",
-    margin: "10px",
-    fontFamily: "roboto",
-    background: "#424854"
-  }
+    width: '300px',
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '10px',
+    fontFamily: 'roboto',
+    background: '#424854',
+  };
   const boutonStyle = {
-    background: "#11ffee00",
-    border: "1px solid white",
-    color: "white",
-    borderRadius: "5px"
-  }
+    background: '#11ffee00',
+    border: '1px solid white',
+    color: 'white',
+    borderRadius: '5px',
+  };
 
   const declineBouton = {
-    background: "#11ffee00",
-    border: "1px solid white",
-    color: "white",
-    borderRadius: "5px",
-    float: "right"
-  }
-    const redirect = useNavigate();
+    background: '#11ffee00',
+    border: '1px solid white',
+    color: 'white',
+    borderRadius: '5px',
+    float: 'right',
+  };
+  const redirect = useNavigate();
 
   return (
     <div>
       <div className="accueil">
         <p id="head1" className="header tracking-in-expand-fwd">
-          Hello ! <br /><br /> Je m'appelle Guilhem SEYVET
+          Hello ! <br />
+          <br /> Je m'appelle Guilhem SEYVET
         </p>
         <p id="head2" className="header tracking-in-expand-fwd2">
           Je suis développeur web full-stack
         </p>
-        <button type="button" className="button-accueil text-focus-in" onClick={() => redirect("/about")}>Continuer</button>
+        <button
+          type="button"
+          className="button-accueil text-focus-in"
+          onClick={() => redirect('/about')}
+        >
+          Continuer
+        </button>
         <div class="light x1"></div>
         <div class="light x2"></div>
         <div class="light x3"></div>
@@ -48,7 +55,23 @@ export function Accueil() {
         <div class="light x8"></div>
         <div class="light x9"></div>
       </div>
-      <CookieConsent style={cookieStyle} buttonText={"J'accepte"} buttonStyle={boutonStyle} declineButtonStyle={declineBouton}enableDeclineButton declineButtonText={"Je refuse"}>🍪 Ce site utilise des cookies pour améliorer l'experience de l'utilisateur.</CookieConsent>
+      <div className="cookies">
+      <CookieConsent
+        buttonClasses="butonCookie"
+        declineButtonClasses="btnRefuse"
+        containerClasses="cookie"
+        overlayClasses="overlay"
+        contentClasses="text-cookie"
+        buttonText={"J'accepte"}
+        disableStyles={true}
+        enableDeclineButton
+        buttonWrapperClasses="divbuton"
+        declineButtonText={'Je refuse'}
+      >
+        🍪 Ce site utilise des cookies pour améliorer l'experience de
+        l'utilisateur.
+      </CookieConsent>
+      </div>
     </div>
   );
 }
