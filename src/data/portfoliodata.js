5c22d64c07a5fafeb4c68aab1bbbceab8a2570d7
Guilhem SEYVET <guilhemcvdev@outlook.com>
2022-01-25 10:54:43 +0100

    modification data portfolio

--- src/data/portfoliodata.js
+++ src/data/portfoliodata.js
@@ -1,32 +1,67 @@
 import swiper from '../assets/images/swiper.png';
-import hackathon from "../assets/images/hackathon.png";
-import panam from "../assets/images/panam.png";
-import hackaton2 from "../assets/images/hackathon2.jpg";
+import hackathon from '../assets/images/hackathon.png';
+import panam from '../assets/images/panam.png';
+import hackaton2 from '../assets/images/hackathon2.jpg';
 
 const portfoliodata = [
   {
     image: hackaton2,
-    lien: "https://lesroismerlin-manomano.netlify.app/",
-    description: "Deuxième Hackathon en collaboration avec Mano Mano. Objectif, réaliser une amélioration d'une partie du site ou une feature liée aux services du site. Nous avons développé une fonctionnalité de mise en relation de particulier, et une fonctionnalité pour proposer des tutoriels aux utilisateurs.",
-  },
-  {
-    image: swiper,
-    lien: "https://swiper-nantes.netlify.app/",
+    lien: 'https://lesroismerlin-manomano.netlify.app/',
     description:
-      "Dans le cadre de mon deuxième projet pour la Wild Code School, nous avons réalisé un site qui répertorie les musées, restaurants et salles de spectacles pour les sorties a Nantes. Ces activités sont répertoriées sous forme de 'swipe card' et en intégration sur une Google map.",
+      "Deuxième Hackathon en collaboration avec Mano Mano. Objectif, réaliser une amélioration d'une partie du site ou une feature liée aux services du site. Nous avons développé une fonctionnalité de mise en relation de particulier, et une fonctionnalité pour proposer des tutoriels aux utilisateurs.",
+    github: 'https://github.com/guilhemcv/hackaton_2',
+    duree: '48h', 
+    personnes: 4,
+    date: "Jan 2022",
+    technos1: "ReactJS",
+    technos2:  "Tailwind",
+    technos3: "Bootstrap",
+    technos4:  "CSS",
+    technos5:  "Github",
   },
   {
     image: hackathon,
-    lien: "https://wildvibes.netlify.app/",
+    lien: 'https://wildvibes.netlify.app/',
     description:
       "Hackathon réalisé en 36h sur le thème de la musique (projet vainqueur). En partant du postulat que les personnes sourdes ou malentendantes ne pouvaient pas écouter de musique, nous avons développé une application permettant de ressentir la musique par les vibrations du téléphone. Du fait du bridage des terminaux IOS, l'application n'est disponible que pour les appareils android.",
+    github: 'https://github.com/guilhemcv/hackathon-novembre2021',
+    date: 'Nov 2021',
+    duree: '36h', 
+    personnes: 4,
+    technos1: "ReactJS",
+    technos2:  "Framer Motion",
+    technos3: "Bootstrap",
+    technos4:  "CSS",
+    technos5:  "Github",
+  },
+  {
+    image: swiper,
+    lien: 'https://swiper-nantes.netlify.app/',
+    description:
+      "Dans le cadre de mon deuxième projet pour la Wild Code School, nous avons réalisé un site qui répertorie les musées, restaurants et salles de spectacles pour les sorties a Nantes. Ces activités sont répertoriées sous forme de 'swipe card' et en intégration sur une Google map.",
+    github: 'https://github.com/guilhemcv/swiper',
+    date: 'Oct 2021',
+    duree: '1 mois', 
+    personnes: 4,
+    technos1: "ReactJS",
+    technos2:  "Rest API",
+    technos3: "Google Cloud Console",
+    technos4:  "CSS",
+    technos5:  "Github",
   },
   {
     image: panam,
-    lien: "https://guilhemcv.github.io/panam-web/",
+    lien: 'https://guilhemcv.github.io/panam-web/',
     description:
-      "Premier projet, après 3 semaines de formation. Site statique en HTML, choix du thème libre.",
-  }
+      'Premier projet, après 3 semaines de formation. Site statique en HTML, choix du thème libre.',
+    github: 'https://github.com/guilhemcv/panam-web',
+    date: 'Sept 2021',
+    duree: '1 mois', 
+    personnes: 4,
+    technos1: "HTML",
+    technos2:  "CSS",
+    technos3:  "Github",
+  },
 ];
 
 export default portfoliodata;
