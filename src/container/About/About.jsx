e02c3e750a56a4eb0c8822df125ab858ebd74c78
Guilhem SEYVET <guilhemcvdev@outlook.com>
2022-04-03 11:05:14 +0200

    ajout skills

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,24 +1,15 @@
 import ScrollAnimation from 'react-animate-on-scroll';
 import { Parallax } from 'react-parallax';
+import { hardSkills } from '../../data/hardSkills';
 import 'animate.css/animate.min.css';
 import './About.css';
 import '../Contact/Contact.css';
 import Polygon from '../../components/Polygon/Polygon';
 import Softskill from '../../data/softskill';
-import javascript from '../../assets/images/javascript.png';
-import react from '../../assets/images/react.png';
-import html from '../../assets/images/html.png';
-import git from '../../assets/images/git.png';
-import mysql from '../../assets/images/mysql.png';
 import guilhem from '../../assets/images/portrait.jpg';
-import github from '../../assets/images/github.png';
-import linkedin from '../../assets/images/linkedin.png';
-import node from '../../assets/images/node.png';
-import notion from '../../assets/images/notion.png';
 import ScrollToTop from '../../components/ScrollToTop/ScrollToTop';
-import css from '../../assets/images/css.png';
-import figma from '../../assets/images/figma.png';
-import api from '../../assets/images/api.png';
+import linkedin from '../../assets/images/linkedin.png';
+import github from '../../assets/images/github.png';
 
 export function About() {
   return (
@@ -28,89 +19,27 @@ export function About() {
           <h1 className="about-titre">A PROPOS</h1>
           <h2 className="softskills">Soft Skills</h2>
         </ScrollAnimation>
-        <div className="skills">
-          {Softskill.map((skill) => (
-            <Polygon skill={skill} />
-          ))}
-        </div>
+        <ScrollAnimation animateIn="animate__fadeIn" animateOnce={true}>
+          <div className="skills">
+            {Softskill.map((skill) => (
+              <Polygon skill={skill} />
+            ))}
+          </div>
+        </ScrollAnimation>
       </div>
       <div className="about-middle">
         <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
           <p className="softskills2">Skills</p>
         </ScrollAnimation>
         <div className="skill-percent">
-          <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
-            <div className="skills-card">
-              <p className="text-skill">Javascript</p>
-              <img width="50px" src={javascript} alt={javascript} />
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
-            <div className="skills-card">
-              <p className="text-skill">React</p>
-              <img width="50px" src={react} alt={react} />
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
-            <div className="skills-card">
-              <p className="text-skill">NodeJS</p>
-              <img width="50px" src={node} alt={node} />
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
-            <div className="skills-card">
-              <p className="text-skill">ExpressJS</p>
-              <img width="50px" src={node} alt={node} />
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
-            <div className="skills-card">
-              <p className="text-skill">HTML</p>
-              <img width="50px" src={html} alt={html} />
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
-            <div className="skills-card">
-              <p className="text-skill">CSS</p>
-              <img width="50px" src={css} alt={css} />
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
-            <div className="skills-card">
-              <p className="text-skill">MySQL</p>
-              <img width="50px" src={mysql} alt={mysql} />
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
-            <div className="skills-card">
-              <p className="text-skill">Rest Api</p>
-              <img width="50px" src={api} alt={api} />
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
-            <div className="skills-card">
-              <p className="text-skill">Git</p>
-              <img width="50px" src={git} alt={git} />
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
-            <div className="skills-card">
-              <p className="text-skill">Github</p>
-              <img width="50px" src={github} alt={github} />
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
-            <div className="skills-card">
-              <p className="text-skill">Figma</p>
-              <img width="50px" src={figma} alt={figma} />
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
-            <div className="skills-card">
-              <p className="text-skill">Notion</p>
-              <img width="50px" src={notion} alt={notion} />
-            </div>
-          </ScrollAnimation>
+          {hardSkills.map((skill) => (
+            <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
+              <div className="skills-card">
+                <p className="text-skill">{skill.nom}</p>
+                <img width="50px" src={skill.image} alt={skill.nom} />
+              </div>
+            </ScrollAnimation>
+          ))}
         </div>
       </div>
       <div className="about-bas">
@@ -131,14 +60,23 @@ export function About() {
           </span>
           <ScrollAnimation animateIn="animate__zoomIn" animateOnce={true}>
             <div className="pres-reseau">
-              <p className="presentation"><br/><br/>
+              <p className="presentation">
+                <br />
+                <br />
                 Après plusieurs années d'expérience dans le commerce et en
                 agence de voyages, j'ai souhaité me reconvertir dans la création
                 et la conception de sites internet. Une formation certifiante
                 ainsi qu'une grande volonté m'ont permis d'acquérir rapidement
                 toutes les connaissances techniques requises et de les appliquer
-                dans ce cadre, sur des projets professionnels concluants. <br/> En complément de ma formation, un stage de 5 mois à partir de Février 2022 en tant qu'assistant formateur au sein de la Wild Code School va renforcer mes connaissances et développer ma pédagogie. <br/><br/>J'aimerais à présent mettre mes nouvelles compétences et toute
-                mon expérience au service d'une équipe et je cherche donc un poste en tant que développeur web.
+                dans ce cadre, sur des projets professionnels concluants. <br />{' '}
+                En complément de ma formation, un stage de 5 mois à partir de
+                Février 2022 en tant qu'assistant formateur au sein de la Wild
+                Code School va renforcer mes connaissances et développer ma
+                pédagogie. <br />
+                <br />
+                J'aimerais à présent mettre mes nouvelles compétences et toute
+                mon expérience au service d'une équipe et je cherche donc un
+                poste en tant que développeur web.
               </p>
               <span className="reseau">
                 <a
@@ -163,13 +101,13 @@ export function About() {
         </div>
         <ScrollToTop />
         <div className="copyright">
-        <p className="copy">
-          Copyright © 2021 - 2022 / Guilhem Seyvet - Développeur web{' '}
-        </p>
-        <a href="mentions.html" target="_blank">
-          Mentions Légales
-        </a>
-      </div>
+          <p className="copy">
+            Copyright © 2021 - 2022 / Guilhem Seyvet - Développeur web{' '}
+          </p>
+          <a href="mentions.html" target="_blank">
+            Mentions Légales
+          </a>
+        </div>
       </div>
     </div>
   );
---
+++ src/data/hardSkills.js
@@ -0,0 +1,74 @@
+import javascript from '../assets/images/javascript.png';
+import react from '../assets/images/react.png';
+import html from '../assets/images/html.png';
+import git from '../assets/images/git.png';
+import mysql from '../assets/images/mysql.png';
+import node from '../assets/images/node.png';
+import notion from '../assets/images/notion.png';
+import css from '../assets/images/css.png';
+import figma from '../assets/images/figma.png';
+import api from '../assets/images/api.png';
+import next from '../assets/images/nextjs.png';
+import prisma from '../assets/images/prisme.png';
+import github from '../assets/images/github.png';
+
+
+
+export const hardSkills = [
+  {
+    nom: 'Javascript',
+    image: javascript,
+  },
+  {
+    nom: 'React',
+    image: react,
+  },
+  {
+    nom: 'NodeJS',
+    image: node,
+  },
+  {
+    nom: 'ExpressJS',
+    image: node,
+  },
+  {
+    nom: 'NextJS',
+    image: next,
+  },
+  {
+    nom: 'Prisma',
+    image: prisma,
+  },
+  {
+    nom: 'HTML',
+    image: html,
+  },
+  {
+    nom: 'CSS',
+    image: css,
+  },
+  {
+    nom: 'MySQL',
+    image: mysql,
+  },
+  {
+    nom: 'Rest Api',
+    image: api,
+  },
+  {
+    nom: 'Git',
+    image: git,
+  },
+  {
+    nom: 'Github',
+    image: github,
+  },
+  {
+    nom: 'Figma',
+    image: figma,
+  },
+  {
+    nom: 'Notion',
+    image: notion,
+  },
+];
